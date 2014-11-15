af913115344781c76964dc9dda8ae56e39d96bb8
surrealcristian <surrealcristian@gmail.com>
2014-11-15 19:48:04 -0300

    Changed class name.

--- memeapi/__init__.py
+++ memeapi/__init__.py
@@ -8,4 +8,4 @@ memeapi is a library for python that wraps the memegenerator.net API.
 __author__ = 'Cristian Cabrera <surrealcristian@gmail.com>'
 __version__ = '0.1'
 
-from .memeapi import MemeGeneratorAPI
+from .memeapi import MemeAPI
