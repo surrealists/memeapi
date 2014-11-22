deb8476ba64fc555f037c66cd0bd5c1bd7d2e745
surrealcristian <surrealcristian@gmail.com>
2014-11-22 15:34:53 -0300

    Module memeapi renamed to api.

--- memeapi/__init__.py
+++ memeapi/__init__.py
@@ -8,7 +8,7 @@ memeapi is a library for python that wraps the memegenerator.net API.
 __author__ = 'Cristian Cabrera <surrealcristian@gmail.com>'
 __version__ = '0.2.0'
 
-from .memeapi import (
+from .api import (
     generators_search, generators_select_by_popular, generators_select_by_new,
     generators_select_by_trending, generators_select_related_by_display_name,
     generators_select_by_url_name_or_generator_id, instances_select_by_popular,
