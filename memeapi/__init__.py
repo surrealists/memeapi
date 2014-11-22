bec68938a0001deee2fc77b56e498ef94c29892d
surrealcristian <surrealcristian@gmail.com>
2014-11-22 15:30:57 -0300

    Class MemeAPI deleted. Methods becomes functions in package memeapi.

--- memeapi/__init__.py
+++ memeapi/__init__.py
@@ -6,6 +6,12 @@ memeapi is a library for python that wraps the memegenerator.net API.
 """
 
 __author__ = 'Cristian Cabrera <surrealcristian@gmail.com>'
-__version__ = '0.1'
+__version__ = '0.2.0'
 
-from .memeapi import MemeAPI
+from .memeapi import (
+    generators_search, generators_select_by_popular, generators_select_by_new,
+    generators_select_by_trending, generators_select_related_by_display_name,
+    generators_select_by_url_name_or_generator_id, instances_select_by_popular,
+    instances_create, instances_select_by_new, instances_select,
+    content_flag_create
+)
